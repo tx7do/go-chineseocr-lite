e20d5cde0c833df032d878575404e79b5d32a9fb
tx7do <yanglinbo@gmail.com>
2023-02-15 23:04:32 +0800

    feat: change value.

--- examples/main.go
+++ examples/main.go
@@ -12,8 +12,8 @@ func main() {
 	var numThread = 4
 	var padding = 50
 	var maxSideLen = 1024
-	var boxScoreThresh float32 = 0.6
-	var boxThresh float32 = 0.3
+	var boxScoreThresh float32 = 0.618
+	var boxThresh float32 = 0.300
 	var unClipRatio float32 = 2.0
 	var doAngle = true
 	var mostAngle = true
@@ -29,7 +29,7 @@ func main() {
 		"../models/keys.txt",
 	)
 
-	result := pred.Detect("_fixtures/", "2.jpg",
+	result := pred.Detect("_fixtures/", "1.jpg",
 		padding, maxSideLen,
 		boxScoreThresh, boxThresh, unClipRatio,
 		doAngle, mostAngle,
